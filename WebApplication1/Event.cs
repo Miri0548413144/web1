6882ff775b11bb9a8cda58e55417b7f13d173b68
AyalaBerman <berman4499@gmail.com>
2024-02-08 13:32:02 +0200

    fix

--- WebApplication1/Event.cs
+++ WebApplication1/Event.cs
@@ -2,14 +2,14 @@
 {
     public class Event
     {
-<<<<<<< HEAD
+
         public int Idch { get; set; }
         public string Title { get; set; }
-=======
+
         public int Id { get; set; }
         public string TitleMiri { get; set; }
         public DateTime Date { get; set; }
->>>>>>> origin/main
+
         
     }
 }
