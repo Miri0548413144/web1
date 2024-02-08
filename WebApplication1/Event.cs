7989119749f4c35c51131b872e6e1333f97090b1
TamarStefansky <ts058320350@gmail.com>
2024-02-08 13:40:22 +0200

    merge confilct

--- WebApplication1/Event.cs
+++ WebApplication1/Event.cs
@@ -2,15 +2,9 @@
 {
     public class Event
     {
-<<<<<<< HEAD
+
         public int IdMiri { get; set; }
-=======
-
-        public int Idch { get; set; }
         public string Title { get; set; }
-
-        public int Id { get; set; }
->>>>>>> origin/main
         public string TitleMiri { get; set; }
         public DateTime Date { get; set; }
 
